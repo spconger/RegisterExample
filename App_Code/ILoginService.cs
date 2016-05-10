87a05f55577358dd45ad856a859041f544d13131
Steve Conger <spconger@gmail.com>
2016-05-10 12:22:41 -0700

    Commit Login service

---
+++ App_Code/ILoginService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ILoginService" in both code and config file together.
+[ServiceContract]
+public interface ILoginService
+{
+   /*
+    This interface has methods for registering a reviewer
+    and for letting the reviewer login
+    You may want to create a DataContract for ShowTracker
+    because it writes to two tables Venue and VenueLogin
+    */
+    [OperationContract]
+    bool RegisterReviewer(Reviewer r);
+
+    [OperationContract]
+    int ReviewerLogin(string userName, string password);
+}
---
+++ App_Code/LoginService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "LoginService" in code, svc and config file together.
+public class LoginService : ILoginService
+{
+
+    BookReviewDbEntities db = new BookReviewDbEntities();
+    public bool RegisterReviewer(Reviewer r)
+    {
+        //this registers a new reviewer using
+        //the stored procedure usp_NewReviewer
+        //the stored procedure takes the fields listed
+        //as parameters. It returns 0 if good
+        //and -1 if the registration fails
+        //usernames and emails must be unique
+        bool result = true;
+        int pass = db.usp_NewReviewer
+            (r.ReviewerUserName,
+            r.ReviewerFirstName,
+            r.ReviewerLastName,
+            r.ReviewerEmail,
+            r.ReviewPlainPassword);
+        if (pass == -1)
+        {
+            result = false;
+        }
+
+        return result;
+    }
+
+    public int ReviewerLogin(string userName, string password)
+    {
+        //logs in a reviewer using the stored Procedure
+        //usp_Reviewer Login
+        // returns 0 if succeeds
+        //-1 if it fails. (not the same as the reviewerKey
+        //which is also set to 0)
+        //when
+        int reviewerKey = 0;
+        int result = db.usp_ReviewerLogin(userName, password);
+        if (result != -1)
+        {
+            var key = (from r in db.Reviewers
+                      where r.ReviewerUserName.Equals(userName)
+                      select new { r.ReviewerKey }).FirstOrDefault();
+
+            reviewerKey = key.ReviewerKey;
+
+          }
+        return reviewerKey;
+
+    }
+}
